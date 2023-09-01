122fdab02f509465b5671277f5292f81875bc7d1
macbook Pro <macbookpro@MacBook-Pro-de-macbook.local>
2023-09-01 11:17:20 -0300

    Webstories

--- index.php
+++ index.php
@@ -4,7 +4,13 @@ get_header();
 if ( is_home() ) {
 	get_template_part( 'template_partes/pagina-inicial' );
 } elseif ( is_category() ) {
-	get_template_part( 'template_partes/categorias' );
+	$categoria = get_queried_object();
+
+	if($categoria->slug == 'webstories') {
+		get_template_part( 'template_partes/webstories' );
+	} else {
+		get_template_part( 'template_partes/categorias' );
+	}
 } elseif ( is_single() ) {
 	get_template_part( 'template_partes/single' );
 } elseif ( is_page() ) {
---
+++ template_partes/webstories.php
@@ -0,0 +1,142 @@
+<div class="barra-busca py-2">
+    <div class="container">
+        <div class="row row-cols-2">
+            <div class="item">
+                <a href="#" class="nome">TEMAS <i></i></a>
+
+                <div class="busca-abre"></div>
+            </div>
+
+            <div class="item">
+                <a href="#" class="nome open-formato">FORMATOS <i></i></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="busca-abre abre-formato">
+        <div class="int p-4">
+            <div class="d-flex flex-column ">
+                <a href="" class="text-uppercase">reportagem</a>
+                <a href="" class="text-uppercase">entrevista</a>
+                <a href="" class="text-uppercase">podcast</a>
+                <a href="" class="text-uppercase">vídeo</a>
+                <a href="" class="text-uppercase">opinião</a>
+                <a href="" class="text-uppercase">webstories</a>
+                <a href="" class="text-uppercase">narrativas visuais</a>
+                <a href="" class="text-uppercase">editorial</a>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div class="marco-result container">
+    <div class="marco-result__destaque position-relative">
+
+        <div class="row row-cols-2">
+            <div class="d-flex flex-column texto">
+                <h1 class="m-0 text-uppercase mt-3">webstories</h1>
+            </div>
+        </div>
+
+	    <div class="marco-result__listagem listagem mx-5 mt-5">
+		    <div class="row row-cols-4 listagem__int">
+			    <div class="listagem-post">
+				    <a href="">
+					    <img src="<?php echo get_template_directory_uri() ?>/assets/images/stories1.png" alt="" class="w-100">
+				    </a>
+			    </div>
+
+			    <div class="listagem-post">
+				    <a href="">
+					    <img src="<?php echo get_template_directory_uri() ?>/assets/images/stories2.png" alt="" class="w-100">
+				    </a>
+			    </div>
+
+			    <div class="listagem-post">
+				    <a href="">
+					    <img src="<?php echo get_template_directory_uri() ?>/assets/images/stories1.png" alt="" class="w-100">
+				    </a>
+			    </div>
+
+			    <div class="listagem-post">
+				    <a href="">
+					    <img src="<?php echo get_template_directory_uri() ?>/assets/images/stories2.png" alt="" class="w-100">
+				    </a>
+			    </div>
+
+			    <div class="listagem-post">
+				    <a href="">
+					    <img src="<?php echo get_template_directory_uri() ?>/assets/images/stories1.png" alt="" class="w-100">
+				    </a>
+			    </div>
+
+			    <div class="listagem-post">
+				    <a href="">
+					    <img src="<?php echo get_template_directory_uri() ?>/assets/images/stories2.png" alt="" class="w-100">
+				    </a>
+			    </div>
+
+			    <div class="listagem-post">
+				    <a href="">
+					    <img src="<?php echo get_template_directory_uri() ?>/assets/images/stories1.png" alt="" class="w-100">
+				    </a>
+			    </div>
+
+			    <div class="listagem-post">
+				    <a href="">
+					    <img src="<?php echo get_template_directory_uri() ?>/assets/images/stories2.png" alt="" class="w-100">
+				    </a>
+			    </div>
+
+			    <div class="listagem-post">
+				    <a href="">
+					    <img src="<?php echo get_template_directory_uri() ?>/assets/images/stories1.png" alt="" class="w-100">
+				    </a>
+			    </div>
+
+			    <div class="listagem-post">
+				    <a href="">
+					    <img src="<?php echo get_template_directory_uri() ?>/assets/images/stories2.png" alt="" class="w-100">
+				    </a>
+			    </div>
+
+			    <div class="listagem-post">
+				    <a href="">
+					    <img src="<?php echo get_template_directory_uri() ?>/assets/images/stories1.png" alt="" class="w-100">
+				    </a>
+			    </div>
+
+			    <div class="listagem-post">
+				    <a href="">
+					    <img src="<?php echo get_template_directory_uri() ?>/assets/images/stories2.png" alt="" class="w-100">
+				    </a>
+			    </div>
+
+			    <div class="listagem-post">
+				    <a href="">
+					    <img src="<?php echo get_template_directory_uri() ?>/assets/images/stories1.png" alt="" class="w-100">
+				    </a>
+			    </div>
+
+			    <div class="listagem-post">
+				    <a href="">
+					    <img src="<?php echo get_template_directory_uri() ?>/assets/images/stories2.png" alt="" class="w-100">
+				    </a>
+			    </div>
+
+			    <div class="listagem-post">
+				    <a href="">
+					    <img src="<?php echo get_template_directory_uri() ?>/assets/images/stories1.png" alt="" class="w-100">
+				    </a>
+			    </div>
+
+			    <div class="listagem-post">
+				    <a href="">
+					    <img src="<?php echo get_template_directory_uri() ?>/assets/images/stories2.png" alt="" class="w-100">
+				    </a>
+			    </div>
+		    </div>
+	    </div>
+    </div>
+</div>
