dd19ea9af0ae83d69947279a7dbd9d4493f34326
macbook Pro <macbookpro@MacBook-Pro-de-macbook.local>
2023-09-20 16:17:59 -0300

    Animacao menu

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -249,13 +249,16 @@ $(window).scroll(function () {
     //desce
     if(scrollY > lastScrollTop) {
         //menu footer na home
-        if($('.menu-footer-home').length > 0 && isVisible($(".menu-footer-home"), 40) && scrollY > 500 && !$('.menu-footer-home').hasClass('less')) {
-            $('.menu-footer-home').addClass('less');
+        if($(window).width() > 768) {
+            if($('.menu-footer-home').length > 0 && isVisible($(".menu-footer-home"), 40) && scrollY > 500 && !$('.menu-footer-home').hasClass('less')) {
+                $('.menu-footer-home').addClass('less');
 
-            if($(window).width() <= 768) {
-                setInterval(footerHome, 3000);
+                // if($(window).width() <= 768) {
+                //     setInterval(footerHome, 3000);
+                // }
             }
         }
+
 
         if(isVisible($(".marco-footer"),-700) && !$('.menu-footer-home').hasClass('hiden')) {
             $('.menu-footer-home').addClass('hiden');
