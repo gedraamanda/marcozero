9ac011b04ba445058b1c783f7a2b312165cef601
macbook Pro <macbookpro@MacBook-Pro-de-macbook.local>
2024-01-09 12:22:49 -0300

    Ajuste usuario inativo

--- tpl-quemfaz.php
+++ tpl-quemfaz.php
@@ -41,32 +41,34 @@ $users = get_users( array(
                 <?php foreach ( $users as $user ) {
 	                $us = get_user_meta( $user->ID );
 	                $usuarioAvatar  = get_avatar_url( $user->ID, array( 'size' => 220 ) );
+                    $inativo = get_field('user_inativo', 'user_'.$user->ID);
 
+                    if(empty($inativo) || $inativo === false) { ?>
+                        <div class="pessoa">
+                            <div class="pessoa__img text-center py-4">
+                                <picture>
+                                    <img src="<?php echo $usuarioAvatar ?>" loading="lazy" />
+                                </picture>
+                            </div>
 
-	                ?>
-                    <div class="pessoa">
-                        <div class="pessoa__img text-center py-4">
-                            <picture>
-                                <img src="<?php echo $usuarioAvatar ?>" loading="lazy" />
-                            </picture>
-                        </div>
+                            <div class="pessoa__txt mt-2">
+                                <div class="d-flex flex-column">
+                                    <p class="nome m-0"><?php echo $user->data->display_name ?></p>
 
-                        <div class="pessoa__txt mt-2">
-                            <div class="d-flex flex-column">
-                                <p class="nome m-0"><?php echo $user->data->display_name ?></p>
+				                    <?php if ( ! empty( $us['description'] ) ) {
+					                    $out = strlen($us['description'][0]) > 100 ? substr($us['description'][0],0,100)."..." : $us['description'][0];
+					                    ?>
+                                        <p class="desc w-75"><?php echo $out ?></p>
+				                    <?php } ?>
 
-	                            <?php if ( ! empty( $us['description'] ) ) {
-		                            $out = strlen($us['description'][0]) > 100 ? substr($us['description'][0],0,100)."..." : $us['description'][0];
-	                                ?>
-                                    <p class="desc w-75"><?php echo $out ?></p>
-	                            <?php } ?>
-
-                                <div>
-                                    <a href="<?php echo get_author_posts_url( $user->ID ) ?>" class="btn">TEXTOS PUBLICADOS</a>
+                                    <div>
+                                        <a href="<?php echo get_author_posts_url( $user->ID ) ?>" class="btn">TEXTOS PUBLICADOS</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    <?php } ?>
+
                 <?php } ?>
             </div>
         </div>
