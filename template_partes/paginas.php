fcffffa66e6047fa67066c4304790fcab30017e3
macbook Pro <macbookpro@MacBook-Pro-de-macbook.local>
2024-01-09 15:48:15 -0300

    Ajuste titulo pagina

--- template_partes/paginas.php
+++ template_partes/paginas.php
@@ -12,27 +12,31 @@ $img_destaqueDesk = get_image_prod( get_the_ID(), 'large' );
 <div class="paginas mb-5">
 	<div class="paginas__int container mt-5">
 		<div class="row conteudo">
-            <div class="col-12 col-md-4 header">
+            <div class="col-12 col-md-7 header">
                 <h1 class="m-0 text-uppercase"><?php the_title(); ?></h1>
             </div>
 
-			<div class="col-12 col-md-7 conteudo__texto marco-single__conteudo">
-                <div class="texto mt-5">
-	                <?php
-	                if ( has_blocks( $post->post_content ) ) {
-		                $blocks = parse_blocks( $post->post_content );
 
-		                foreach ( $blocks as $block ) {
-			                echo apply_filters( 'the_content', render_block( $block ) );
-		                }
-	                } else { //legado
-		                echo apply_filters( 'the_content', $post->post_content );
-	                }
-	                ?>
+		</div>
+
+        <div class="row conteudo">
+            <div class="col-12 col-md-7 ms-auto conteudo__texto marco-single__conteudo">
+                <div class="texto mt-5">
+			        <?php
+			        if ( has_blocks( $post->post_content ) ) {
+				        $blocks = parse_blocks( $post->post_content );
+
+				        foreach ( $blocks as $block ) {
+					        echo apply_filters( 'the_content', render_block( $block ) );
+				        }
+			        } else { //legado
+				        echo apply_filters( 'the_content', $post->post_content );
+			        }
+			        ?>
                 </div>
 
-			</div>
-		</div>
+            </div>
+        </div>
 	</div>
 </div>
 
